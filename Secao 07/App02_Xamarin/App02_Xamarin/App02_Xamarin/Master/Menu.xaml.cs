60baaa716886fb1587ecca733f2675229054c9a7
dev <dev@DESKTOP-BKJPHKC>
2018-04-20 13:58:25 -0300

    Inicio do APP Demonstrativo

---
+++ Secao 07/App02_Xamarin/App02_Xamarin/App02_Xamarin/Master/Menu.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App02_Xamarin.Master
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Menu : MasterDetailPage
+	{
+		public Menu ()
+		{
+			InitializeComponent ();
+		}
+
+        public void GoPaginaPerfil1(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Pages.Perfil1());
+        }
+
+        public void GoPaginaXamarin(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Pages.Xamarin());
+        }
+
+    }
+}
